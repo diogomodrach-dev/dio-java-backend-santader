95d3b81277aefaf15da1684fd5621d6236248415
diogomodrach-dev <diogomodrach.dev@gmail.com>
2024-07-24 12:27:59 -0300

    Anatomia das Classes

---
+++ anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+        
+        final String BR = "Brasil";
+
+        int ano = 2024;
+
+        boolean verdadeira = true;
+
+        System.out.println("Ola, Mundo!! Estamos em " + ano +"\n");
+
+        System.out.println(BR);
+    }
+}
